c5b1e269c570943c0dd474ed2b27a52b43a95cea
Faishal Saiyed <saiyedfaishal@gmail.com>
2016-06-21 17:50:10 +0530

    Removed unwated @uses from doc and flush_rewrite_rule function

--- includes/functions/core.php
+++ includes/functions/core.php
@@ -4,9 +4,6 @@ namespace ElasticPress\Stream\Core;
 /**
  * Default setup routine
  *
- * @uses add_action()
- * @uses do_action()
- *
  * @return void
  */
 function setup() {
@@ -40,12 +37,6 @@ function driver( $default_driver ) {
 /**
  * Registers the default textdomain.
  *
- * @uses apply_filters()
- * @uses get_locale()
- * @uses load_textdomain()
- * @uses load_plugin_textdomain()
- * @uses plugin_basename()
- *
  * @return void
  */
 function i18n() {
@@ -57,7 +48,6 @@ function i18n() {
 /**
  * Initializes the plugin and fires an action other plugins can hook into.
  *
- * @uses do_action()
  *
  * @return void
  */
@@ -68,15 +58,12 @@ function init() {
 /**
  * Activate the plugin
  *
- * @uses init()
- * @uses flush_rewrite_rules()
  *
  * @return void
  */
 function activate() {
 	// First load the init scripts in case any rewrite functionality is being loaded
 	init();
-	flush_rewrite_rules();
 }
 
 /**
